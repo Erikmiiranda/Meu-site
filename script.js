f4cf2d1cbc0a3e13ab3a7d0a15d5b2920159ca31
erikmiiranda <erik.miiranda@hotmail.com>
2024-11-08 06:32:32 -0300

    fist commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function exibirMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.textContent = "Parabéns! Você clicou no botão e interagiu com JavaScript!";
+    mensagem.style.color = 'green';
+}
